a3e8ecd5384bccebc1af869128c530f2114a737a
Ranjeet SIngh <32495484+Ranjeet0611@users.noreply.github.com>
2026-08-01 04:49:12 +0530

    feat(window): add Colors

---
+++ kara-ui/Kara/Theme/Colors.swift
@@ -0,0 +1,9 @@
+import SwiftUI
+
+extension Color {
+    static let karaBackground = Color(red: 8 / 255, green: 10 / 255, blue: 14 / 255)
+    static let karaGlass = Color.white.opacity(0.08)
+    static let karaBorder = Color.white.opacity(0.14)
+    static let karaGlow = Color(red: 92 / 255, green: 219 / 255, blue: 255 / 255)
+    static let karaAccent = Color(red: 88 / 255, green: 134 / 255, blue: 255 / 255)
+}
